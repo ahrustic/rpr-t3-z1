c79a4d7970355be5953ff9874cd420f5dc04e36a
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-10-31 21:10:26 +0100

    Update 1

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class FiksniBroj extends TelefonskiBroj{
+
+    private String telBroj;
+    //pokusaj da ne pravim novi niz nego da odmah dodijelim vrijednosti pozivnih brojeva gradu
+    public enum Grad {TRAVNIK (30), ORAŠJE(31), ZENICA(32), SARAJEVO(33), LIVNO (34), TUZLA(35), MOSTAR(36), BIHAĆ(37), GORAŽDE (38), ŠIRKOI_BRIJEG(39), BRČKO(49);
+    private int pozivni;
+        Grad(int i) {
+            this.pozivni = i;
+        }
+      public int vratiPozivni(){
+            return this.pozivni;
+      }
+    };
+    private Grad grad;
+
+    public FiksniBroj (Grad grad, String broj) {
+        this.telBroj = broj;
+        this.grad = grad;
+    }
+
+    @Override
+    public String ispisi() {
+        String ispis = new String();
+        String pozivni = "0" + grad.vratiPozivni();
+        ispis = ispis + pozivni + "/" + telBroj;
+        return ispis;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
