1c74e08aa3fe6c5cd115ed8b035fc075db91719a
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-10-31 21:23:34 +0100

    Update 2 (dodana klasa MobilniBroj)

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj {
+
+    int mobilnaMreza;
+    String broj ;
+
+    MobilniBroj(int mobilnaMreza, String broj) {
+        if (mobilnaMreza >= 60 && mobilnaMreza <= 67) {
+            this.mobilnaMreza = mobilnaMreza;
+            this.broj = broj;
+        }
+        else throw new IllegalArgumentException();
+    }
+
+    @Override
+    public String ispisi() {
+        String ispis = new String();
+        String pozivni = "0" + mobilnaMreza;
+        ispis = ispis + pozivni + "/" + broj;
+        return ispis;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
