f0cde36dbc297139cbdbe132b1262638a48d61b4
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-10-31 21:42:10 +0100

    Update 4 (dodana klasa Imenik)

---
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.Set;
+
+public class Imenik {
+    private String imeVlasnika;
+    TelefonskiBroj brojVlasnika;
+
+    public void dodaj(String ime, TelefonskiBroj broj) {
+
+    }
+
+    String dajBroj(String ime) {
+
+        return brojVlasnika.ispisi();
+    }
+
+    String dajIme (TelefonskiBroj broj) {
+
+        return imeVlasnika;
+    }
+
+    String naSlovo(char s) {
+        return null;
+    }
+     Set<String> izGrada(FiksniBroj.Grad g) {
+        return null;
+    }
+
+    Set<TelefonskiBroj> izGradaBrojevi (FiksniBroj.Grad g) {
+        return null;
+    }
+}
