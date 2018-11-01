fda35a2695c4f0ae7a150ebcaab7596f0f57e9dd
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-11-01 10:26:51 +0100

    popravljena metoda izGrada, zapoceta izGradaBrojeva i ureden enum u fiksnom broju

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -6,12 +6,12 @@ public class FiksniBroj extends TelefonskiBroj{
 
     private String telBroj;
     //pokusaj da ne pravim novi niz nego da odmah dodijelim vrijednosti pozivnih brojeva gradu
-    public enum Grad {TRAVNIK (30), ORAŠJE(31), ZENICA(32), SARAJEVO(33), LIVNO (34), TUZLA(35), MOSTAR(36), BIHAĆ(37), GORAŽDE (38), ŠIRKOI_BRIJEG(39), BRČKO(49);
-    private int pozivni;
-        Grad(int i) {
+    public enum Grad {TRAVNIK ("030"), ORAŠJE("031"), ZENICA("032"), SARAJEVO("033"), LIVNO ("034"), TUZLA("035"), MOSTAR("036"), BIHAĆ("037"), GORAŽDE ("038"), ŠIRKOI_BRIJEG("039"), BRČKO("049");
+    private String pozivni;
+        Grad(String i) {
             this.pozivni = i;
         }
-      public int vratiPozivni(){
+      public String vratiPozivni(){
             return this.pozivni;
       }
     };
@@ -25,7 +25,7 @@ public class FiksniBroj extends TelefonskiBroj{
     @Override
     public String ispisi() {
         String ispis = new String();
-        String pozivni = "0" + grad.vratiPozivni();
+        String pozivni = grad.vratiPozivni();
         ispis = ispis + pozivni + "/" + telBroj;
         //String.format("%d %S / %S - %S");
         return ispis;
--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,14 +1,17 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad;
+
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Imenik {
     private String imeVlasnika;
     TelefonskiBroj brojVlasnika;
     private Map<String,  TelefonskiBroj> mapa;
     public void dodaj(String ime, TelefonskiBroj broj) {
-
+        mapa.putIfAbsent(ime, broj);
     }
 
     String dajBroj(String ime) {
@@ -26,16 +29,28 @@ public class Imenik {
             String ime = el.getKey();
             TelefonskiBroj broj= el.getValue();
             if (ime.charAt(0) == s) {
-                brojeviNaSlovo += "" + redniBroj +" "+ ime + " - " + broj;
+                redniBroj++;
+                brojeviNaSlovo += "" + redniBroj +". "+ ime + " - " + broj;
             }
         }
         return brojeviNaSlovo;
     }
-     Set<String> izGrada(FiksniBroj.Grad g) {
-        return null;
+     Set<String> izGrada(Grad g) {
+        Set<String> brojeviIzGrada = new TreeSet<String>();
+         for (Map.Entry<String, TelefonskiBroj> el:mapa.entrySet()) {
+            TelefonskiBroj broj = el.getValue();
+            if (broj.telBroj.charAt(0) == g.vratiPozivni().charAt(0) && broj.telBroj.charAt(1) == g.vratiPozivni().charAt(1) && broj.telBroj.charAt(2) == g.vratiPozivni().charAt(2))
+                brojeviIzGrada.add(imeVlasnika);
+        }
+        return brojeviIzGrada;
     }
 
-    Set<TelefonskiBroj> izGradaBrojevi (FiksniBroj.Grad g) {
-        return null;
+    Set<TelefonskiBroj> izGradaBrojevi (Grad g) {
+        Set<TelefonskiBroj> brojeviIzGrada = new TreeSet<TelefonskiBroj>();
+        for (Map.Entry<String, TelefonskiBroj> el:mapa.entrySet()) {
+            TelefonskiBroj broj = el.getValue();
+
+        }
+        return brojeviIzGrada;
     }
 }
